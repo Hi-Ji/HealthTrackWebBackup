f99d75665ec31873d6f6f2339a2d5b11951ed121
James777G <hgon777@aucklanduni.ac.nz>
2023-06-27 21:25:41 +1200

    firstChange

--- src/Functions/AimPage.js
+++ src/Functions/AimPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import DiseaseBook from '../Components/AimPage/DiseaseBook.svg'
+import Text from '../data/Text.json'
+
 
 const AimPage = () => {
   return (
     <div style={{margin:'200px auto 0 auto',maxWidth:'1075px',padding:'0 60px'}} >
       <div>
-        <div style={{fontFamily:'Angkor',fontWeight: '300', fontSize:'45px',textAlign:'center',color:'#4DB193'}}>We Aim To</div>
-        <div style={{fontFamily:'Alef',fontWeight: '200',fontSize:'30px',textAlign:'center',color:'black',marginTop:'15px'}}>provide a simple-to-use, aesthetic, and interactive website with vast useful knowledge and information for useusers, as well as raise their awarenesses of health isuues</div>
+        <div style={{fontFamily:'Angkor',fontWeight: '300', fontSize:'45px',textAlign:'center',color:'#4DB193'}}>{Text.Aim}</div>
+        <div style={{fontFamily:'Alef',fontWeight: '200',fontSize:'30px',textAlign:'center',color:'black',marginTop:'15px'}}>{Text.ProvideSimpleToUse}</div>
       </div>
       <div style={{textAlign:'center',marginTop:'250px'}}>
         <img src={DiseaseBook} alt="DiseaseBook" className='diseaseBook' />
