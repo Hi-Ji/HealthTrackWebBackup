4ea1cda3c1c88fd5240feb9353ab3a0d3051ad07
James777G <hgon777@aucklanduni.ac.nz>
2023-06-27 21:09:09 +1200

    changes

--- src/Components/ServicePage/NextButton.js
+++ src/Components/ServicePage/NextButton.js
@@ -3,7 +3,7 @@ import NextButtonSvg from './NextButton.svg'
 
 const NextButton = () => {
   return (
-    <div style={{backgroundColor:'#FDE383',width:'40px',padding:'25px',borderRadius:'100%',marginLeft:'80px',marginTop:'40px'}}>
+    <div style={{backgroundColor:'#FDE383',width:'40px',padding:'25px',borderRadius:'100%',marginLeft:'80px',marginTop:'60px', boxShadow:'0 20px 30px -6px rgba(240, 255, 79, 0.5)'}}>
       
       <img src={NextButtonSvg} alt="NextButtonSvg" />
     </div>
