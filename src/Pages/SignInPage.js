1dda61fbdf58b255368d7004b5c58bba1d88a005
Hi-Ji <zj010110@gmail.com>
2023-07-09 00:45:40 +1200

    update sign page responsive layout

--- src/Pages/SignInPage.js
+++ src/Pages/SignInPage.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import SignInEllipseInfo from "../Functions/SignInPage/SignInEllipseInfo";
 import SignIn from "../Functions/SignInPage/SignIn";
 import "../Styles/SignInPage.css";
+import EllipseSignUpButton from '../Components/SignInPage/Button/EllipseSignUpButton';
+import Text from '../data/Text.json';
+import EllipseLeft from '../Components/SignInPage/Image/ellipseLeft.svg'
+import EllipseRight from '../Components/SignInPage/Image/ellipseRight.svg'
 
 const SignInPage = () => {
   return (
-    <div className="pageSpliter">
+    <Fragment>
+      <div className="pageSpliter">
         <SignInEllipseInfo />
         <SignIn />
-    </div>
+      </div>
+
+      <div className="pageSpliter1">
+        <div className='pageSpliter2'>
+          <div className="newHereText">
+              {Text.SigninNew}
+          </div>
+          <div className="signUpText">
+              {Text.SignIn}
+          </div>
+
+          <EllipseSignUpButton />
+          <div className='EllipseSignUpButtonBGC'></div>
+        </div>
+        
+        <SignIn />
+      </div>
+
+      <div className='ellipseLeftRightImg'>
+        <img src={EllipseLeft} className="ellipseLeftImg1" />
+        <img src={EllipseRight} className="ellipseRightImg1" />
+      </div>
+    </Fragment>
   )
 }
 
--- src/Pages/SignUpPage.js
+++ src/Pages/SignUpPage.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import SignUpEllipseInfo from "../Functions/SignInPage/SignUpEllipseInfo";
+import EllipseSignInButton from '../Components/SignInPage/Button/EllipseSignInButton';
 import SignUp from "../Functions/SignInPage/SignUp";
 import "../Styles/SignInPage.css";
+import Text from '../data/Text.json'
+import EllipseCenter from '../Components/SignInPage/Image/ellipseCenter.svg'
 
 const SignUpPage = () => {
   return (
-    <div className="pageSpliter">
+    // <div className="pageSpliter">
+    //     <SignUpEllipseInfo />
+    //     <SignUp />
+    // </div>
+    <Fragment>
+      <div className="pageSpliter">
         <SignUpEllipseInfo />
         <SignUp />
-    </div>
+      </div>
+
+      <div className="pageSpliter1">
+        <div className='pageSpliter2'>
+          <div className="newHereText">
+              {Text.SigninNew}
+          </div>
+          <div className="signUpText">
+              {Text.SignIn}
+          </div>
+
+          <EllipseSignInButton />
+          <div className='EllipseSignUpButtonBGC'></div>
+        </div>
+        
+        <SignUp />
+      </div>
+
+      <div className='ellipseLeftRightImg'>
+        <img src={EllipseCenter} className="EllipseCenterImg1" />
+      </div>
+    </Fragment>
   )
 }
 
