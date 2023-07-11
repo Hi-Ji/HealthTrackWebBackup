90c6369e361a22dee1905c65fad1093d8dd8f425
James777G <hgon777@aucklanduni.ac.nz>
2023-07-11 14:55:03 +1200

    linking

--- src/Functions/IntroPage/IntroBanner.js
+++ src/Functions/IntroPage/IntroBanner.js
@@ -4,61 +4,70 @@ import DrugInteraction from "../../Components/IntroPage/Image/DrugInteraction.sv
 import SymptonFinder from "../../Components/IntroPage/Image/SymptomFinder.svg";
 import HealthAssessment from "../../Components/IntroPage/Image/HealthAssessment.svg";
 import Notification from "../../Components/IntroPage/Image/Notification.svg";
-import OurTeam from "../../Components/IntroPage/Image/OurTeam.svg"
+import OurTeam from "../../Components/IntroPage/Image/OurTeam.svg";
+import { useNavigate } from "react-router-dom";
 
 
-const IntroBanner = () => (
+const IntroBanner = () => {
+    
+    const navigate = useNavigate();
 
-    <div className="bannerHolder">
-        <div className="firstHalfSplitHolder">
-            <div className="firstLeftVSplitHolder">
-                <div className="firstLeftHSplitHolder">
-                    <div className="lifeStyleText">
-                        {Text.LifeStyle}
+    const goOurTeam = () => {
+        navigate('/ourteam');
+    }
+    
+    return (
+        <div className="bannerHolder">
+            <div className="firstHalfSplitHolder">
+                <div className="firstLeftVSplitHolder">
+                    <div className="firstLeftHSplitHolder">
+                        <div className="lifeStyleText">
+                            {Text.LifeStyle}
+                        </div>
+                        <img src={LifestyleAdvice} className="lifestyleAdviceImg" />
+                        <div style={{ height: "1rem" }}></div>
                     </div>
-                    <img src={LifestyleAdvice} className="lifestyleAdviceImg" />
-                    <div style={{height:"1rem"}}></div>
+                    <div className="secondLeftHSplitHolder">
+                        <div className="drugInteractionText">
+                            {Text.DrugInter}
+                        </div>
+                        <img src={DrugInteraction} className="drugInteractionImg" />
+                        <div style={{ height: "1rem" }}></div>
+                    </div>
                 </div>
-                <div className="secondLeftHSplitHolder">
-                    <div className="drugInteractionText">
-                        {Text.DrugInter}
+                <div className="secondLeftVSplitHolder">
+                    <div className="symptomFinderText">
+                        {Text.SymptonFinder}<br />
+                        {Text.SymptonFinder2}
                     </div>
-                    <img src={DrugInteraction} className="drugInteractionImg" />
-                    <div style={{height:"1rem"}}></div>
+                    <img src={SymptonFinder} className="symptomFinderImg" />
+                    <div style={{ height: "1.5rem" }}></div>
                 </div>
             </div>
-            <div className="secondLeftVSplitHolder">
-                <div className="symptomFinderText">
-                    {Text.SymptonFinder}<br/>
-                    {Text.SymptonFinder2}
+            <div className="secondHalfSplitHolder">
+                <div className="firstRightHSplitHolder">
+                    <div className="healthAssessmentText">
+                        {Text.HealthAssessment}
+                    </div>
+                    <img src={HealthAssessment} className="healthAssessmentImg" />
                 </div>
-                <img src={SymptonFinder} className="symptomFinderImg"/>
-                <div style={{height:"1.5rem"}}></div>
+                <div className="secondRightHSplitHolder">
+                    <div className="firstRightVSplitHolder">
+                        <div className="notificationText">
+                            {Text.Notification}
+                        </div>
+                        <img src={Notification} className="notificationImg" />
+                    </div>
+                    <div className="secondRightVSplitHolder" onClick={goOurTeam}>
+                        <div className="ourTeamText">
+                            {Text.OurTeam}
+                        </div>
+                        <img src={OurTeam} className="ourTeamImg" />
+                    </div>
+                </div>
             </div>
         </div>
-        <div className="secondHalfSplitHolder">
-            <div className="firstRightHSplitHolder">
-                <div className="healthAssessmentText">
-                    {Text.HealthAssessment}
-                </div>
-                <img src={HealthAssessment} className="healthAssessmentImg"/>
-            </div>
-            <div className="secondRightHSplitHolder">
-                <div className="firstRightVSplitHolder">
-                    <div className="notificationText">
-                        {Text.Notification}
-                    </div>
-                    <img src={Notification} className="notificationImg"/>
-                </div>
-                <div className="secondRightVSplitHolder">
-                    <div className="ourTeamText">
-                        {Text.OurTeam}
-                    </div>
-                    <img src={OurTeam} className="ourTeamImg"/>
-                </div>
-            </div>
-        </div>
-    </div>
-)
+    )
+}
 
 export default IntroBanner;
